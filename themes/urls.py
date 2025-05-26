d9cb3ee63e2fdfcb544a539973db09eb38092756
Rajeev <rajeevlakshmanan003@gmail.com>
2025-05-26 18:05:23 +0530

    All pages layout alligned and set routing path and functions.

--- themes/urls.py
+++ themes/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about/', views.about_us, name='about_us'),
+    path('blog/', views.blog, name='blog'),
+    path('contact/', views.contact, name='contact'),
+    path('service/', views.service, name='service'),
 ]
--- themes/views.py
+++ themes/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def about_us(request):
+    return render(request, 'about.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def service(request):
+    return render(request, 'service.html')
