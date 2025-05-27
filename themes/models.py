d8587a70a9c4d9dc306bd502abfa7f8006dd56a3
Rajeev <rajeevlakshmanan003@gmail.com>
2025-05-27 17:39:54 +0530

    User contact form finished.

--- themes/models.py
+++ themes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=25)
+    email = models.EmailField()
+    phone = models.CharField(max_length=10)
+    service = models.CharField(max_length=15)
+    message = models.TextField()
+    date_time = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.name
--- themes/views.py
+++ themes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from . models import Contact
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -11,6 +11,14 @@ def blog(request):
     return render(request, 'blog.html')
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        service = request.POST.get('service')
+        msg = request.POST.get('msg')
+        contact_data = Contact.objects.create(name=name, email=email, phone=phone, service=service, message=msg)
+        contact_data.save()
     return render(request, 'contact.html')
 
 def service(request):
