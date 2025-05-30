5f85b3411883ae439cbfc24ea67003c69d28e00b
Rajeev <rajeevlakshmanan003@gmail.com>
2025-05-30 15:57:56 +0530

    Contact form bug fixed and tested.

--- themes/views.py
+++ themes/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . models import Contact
+from django.contrib import messages
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -12,13 +13,21 @@ def blog(request):
 
 def contact(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        phone = request.POST.get('phone')
-        service = request.POST.get('service')
-        msg = request.POST.get('msg')
-        contact_data = Contact.objects.create(name=name, email=email, phone=phone, service=service, message=msg)
-        contact_data.save()
+        try:
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            phone = request.POST.get('phone')
+            service = request.POST.get('service')
+            msg = request.POST.get('msg')
+            contact_data = Contact.objects.create(name=name, email=email, phone=phone, service=service, message=msg)
+            contact_data.save()
+            success_msg = 'Successfully submitted'
+            messages.success(request, success_msg)
+            return redirect('contact')
+        except Exception as e:
+            error_msg = 'Server unreachable'
+            messages.error(request, error_msg)
+            return redirect('contact')
     return render(request, 'contact.html')
 
 def service(request):
